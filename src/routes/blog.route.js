db2e34f76e84347fd7621d1c412b6b7b5a77eb3b
= <68342984+HealthHighway@users.noreply.github.com>
2022-02-28 18:43:42 +0530

    done

--- src/routes/blog.route.js
+++ src/routes/blog.route.js
@@ -9,4 +9,27 @@ router.get('/', function (req, res) {
     res.send('Welcome to Blog Home Route')
 })
 
+router.get("/:blogId", [
+    param('blogId').exists().withMessage("blogId not found").isMongoId().withMessage("invalid blogId")
+], checkRequestValidationMiddleware, async (req, res) => {
+    
+    try
+    {
+        let blog = await BlogModel.findOne({ url : req.params.bid })
+
+        const randomSuggestions = await BlogModel.aggregate([
+            { $match : { _id : { $ne : req.params.bid } } },
+            { $sample : { size : 2 } }
+        ])
+
+        // const skip = Math.floor(Math.random() * total) + 1;
+        // const suggestions = await BlogModel.find({}).skip(skip).limit(2).exec();
+        jRes(res, 200, {blog, suggestions : randomSuggestions});
+    }catch(err){
+        console.log(err)
+        jRes(res, 400, err)
+    }
+
+})
+
 export default router;
