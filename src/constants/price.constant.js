625ecc606ab287a8a2aefeb7a0ec71d54a895cb1
vyomgupta <vyomgupta456@gmail.com>
2022-04-07 02:07:59 +0530

    done

--- src/constants/price.constant.js
+++ src/constants/price.constant.js
@@ -3,46 +3,46 @@ export const privateSessionPlans = {
     "IN" : {
         "currency" : "INR",
         "plans" : [
-            { title : "Starter", sessions : 1, originalPrice : 99, discountedPrice : 99 },
-            { title : "Yoga Fan", sessions : 16, originalPrice : 2149, discountedPrice : 2149 },
-            { title : "Yoga Buff", sessions : 20, originalPrice : 2449, discountedPrice : 2449 },
-            { title : "Yoga Addict", sessions : 24, originalPrice : 2849, discountedPrice : 2849 },
+            { title : "Starter", sessions : 1, originalPrice : 99, firstTimeDiscountedPrice : 99, discountedPrice : 99 },
+            { title : "Yoga Fan", sessions : 16, originalPrice : 2149, firstTimeDiscountedPrice : 99, discountedPrice : 2149 },
+            { title : "Yoga Buff", sessions : 20, originalPrice : 2449, firstTimeDiscountedPrice : 99, discountedPrice : 2449 },
+            { title : "Yoga Addict", sessions : 24, originalPrice : 2849, firstTimeDiscountedPrice : 99, discountedPrice : 2849 },
         ]
     },
     "US" : {
         "currency" : "USD",
         "plans" : [
-            { title : "Starter", sessions : 1, originalPrice : 99, discountedPrice : 99 },
-            { title : "Yoga Fan", sessions : 16, originalPrice : 2149, discountedPrice : 2149 },
-            { title : "Yoga Buff", sessions : 20, originalPrice : 2449, discountedPrice : 2449 },
-            { title : "Yoga Addict", sessions : 24, originalPrice : 2849, discountedPrice : 2849 },
+            { title : "Starter", sessions : 1, originalPrice : 99, firstTimeDiscountedPrice : 99, discountedPrice : 99 },
+            { title : "Yoga Fan", sessions : 16, originalPrice : 2149, firstTimeDiscountedPrice : 99, discountedPrice : 2149 },
+            { title : "Yoga Buff", sessions : 20, originalPrice : 2449, firstTimeDiscountedPrice : 99, discountedPrice : 2449 },
+            { title : "Yoga Addict", sessions : 24, originalPrice : 2849, firstTimeDiscountedPrice : 99, discountedPrice : 2849 },
         ]
     },
     "ZA" : {
         "currency" : "USD",
         "plans" : [
-            { title : "Starter", sessions : 1, originalPrice : 99, discountedPrice : 99 },
-            { title : "Yoga Fan", sessions : 16, originalPrice : 2149, discountedPrice : 2149 },
-            { title : "Yoga Buff", sessions : 20, originalPrice : 2449, discountedPrice : 2449 },
-            { title : "Yoga Addict", sessions : 24, originalPrice : 2849, discountedPrice : 2849 },
+            { title : "Starter", sessions : 1, originalPrice : 99, firstTimeDiscountedPrice : 99, discountedPrice : 99 },
+            { title : "Yoga Fan", sessions : 16, originalPrice : 2149, firstTimeDiscountedPrice : 99, discountedPrice : 2149 },
+            { title : "Yoga Buff", sessions : 20, originalPrice : 2449, firstTimeDiscountedPrice : 99, discountedPrice : 2449 },
+            { title : "Yoga Addict", sessions : 24, originalPrice : 2849, firstTimeDiscountedPrice : 99, discountedPrice : 2849 },
         ]
     },
     "AU" : {
         "currency" : "USD",
         "plans" : [
-            { title : "Starter", sessions : 1, originalPrice : 99, discountedPrice : 99 },
-            { title : "Yoga Fan", sessions : 16, originalPrice : 2149, discountedPrice : 2149 },
-            { title : "Yoga Buff", sessions : 20, originalPrice : 2449, discountedPrice : 2449 },
-            { title : "Yoga Addict", sessions : 24, originalPrice : 2849, discountedPrice : 2849 },
+            { title : "Starter", sessions : 1, originalPrice : 99, firstTimeDiscountedPrice : 99, discountedPrice : 99 },
+            { title : "Yoga Fan", sessions : 16, originalPrice : 2149, firstTimeDiscountedPrice : 99, discountedPrice : 2149 },
+            { title : "Yoga Buff", sessions : 20, originalPrice : 2449, firstTimeDiscountedPrice : 99, discountedPrice : 2449 },
+            { title : "Yoga Addict", sessions : 24, originalPrice : 2849, firstTimeDiscountedPrice : 99, discountedPrice : 2849 },
         ]
     },
     "DEFAULT_COUNTRY" : {
         "currency" : "USD",
         "plans" : [
-            { title : "Starter", sessions : 1, originalPrice : 99, discountedPrice : 99 },
-            { title : "Yoga Fan", sessions : 16, originalPrice : 2149, discountedPrice : 2149 },
-            { title : "Yoga Buff", sessions : 20, originalPrice : 2449, discountedPrice : 2449 },
-            { title : "Yoga Addict", sessions : 24, originalPrice : 2849, discountedPrice : 2849 },
+            { title : "Starter", sessions : 1, originalPrice : 99, firstTimeDiscountedPrice : 99, discountedPrice : 99 },
+            { title : "Yoga Fan", sessions : 16, originalPrice : 2149, firstTimeDiscountedPrice : 99, discountedPrice : 2149 },
+            { title : "Yoga Buff", sessions : 20, originalPrice : 2449, firstTimeDiscountedPrice : 99, discountedPrice : 2449 },
+            { title : "Yoga Addict", sessions : 24, originalPrice : 2849, firstTimeDiscountedPrice : 99, discountedPrice : 2849 },
         ]
     }
 }
